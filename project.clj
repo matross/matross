28b2ff97d36744ee5b8d8e0d5187af5dac43f22a
Thomas Omans <thomas.omans@gmail.com>
2014-04-21 16:17:59 -0700

    fixing license in project file :older_woman:

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject matross "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description ""
+  :url "http://github.com/matross/matross"
+  :license {:name "MIT"
+            :url "https://github.com/matross/matross/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
