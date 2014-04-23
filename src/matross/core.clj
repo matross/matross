64a74e54a5f041c77fe701fe0526bbbeef2d8b1c
Thomas Omans <thomas.omans@gmail.com>
2014-04-23 08:29:50 -0700

    bringing in option parser :construction:

--- src/matross/core.clj
+++ src/matross/core.clj
@@ -1,5 +1,6 @@
 (ns matross.core
-  (:require [matross.connections.ssh :as ssh]
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [matross.connections.ssh :as ssh]
             [matross.connections.local :as local]
             [matross.connections.core :refer [run!]]
             [matross.model.core :as model])
@@ -9,7 +10,13 @@
                            {:type :ssh :hostname "127.0.0.1"}
                            {:type :local}]})
 
+(def cli-opts
+  [["-h" "--help"]])
+
 (defn -main [& args]
-  (let [{:keys [connections]} (model/prepare config)]
-    (doseq [result (map (partial run! config) connections)]
-      (prn result))))
+  (let [opts (parse-opts args cli-opts)
+        {:keys [connections]} (model/prepare config)]
+    (if (get-in opts [:options :help])
+      (prn "matross!")
+      (doseq [result (map (partial run! config) connections)]
+        (prn result)))))
