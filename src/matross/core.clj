1b0d60456ca1002b44264d80937503cc8e77c218
Darrell Hamilton <darrell.noice@gmail.com>
2014-04-23 21:09:10 -0700

    hacked in sudo passwords for testing

--- src/matross/core.clj
+++ src/matross/core.clj
@@ -2,7 +2,7 @@
   (:require [clojure.tools.cli :refer [parse-opts]]
             [matross.connections.ssh]
             [matross.connections.local]
-            [matross.connections.core :refer [connect disconnect run]]
+            [matross.connections.core :refer [connect disconnect run sudo-runner]]
             [matross.model.core :as model])
   (:gen-class))
 
@@ -11,7 +11,8 @@
                            {:type :local}]})
 
 (def cli-opts
-  [["-h" "--help"]])
+  [["-h" "--help"]
+   ["-K" "--ask-sudo-pass"]])
 
 (defn get-sensitive-user-input [prompt]
   (let [console (. System console)
@@ -20,18 +21,20 @@
       (-> console (.readPassword padded-prompt nil) String/valueOf)
       (throw (Exception. "Could not find system console.")))))
 
-(defn test-run-connection! [conn test-command]
+(defn test-run-connection! [conn test-command pass]
   (connect conn)
-  (let [{exit :exit :as result} (run conn {:cmd [test-command]})]
-    (prn {:exit @exit
-          :out (me.raynes.conch.low-level/stream-to-string result :out)
-          :err (me.raynes.conch.low-level/stream-to-string result :err)}))
+  (let [conn (if pass (sudo-runner conn "root" pass) conn)]
+    (let [{exit :exit :as result} (run conn {:cmd [test-command]})]
+      (prn {:exit @exit
+            :out (me.raynes.conch.low-level/stream-to-string result :out)
+            :err (me.raynes.conch.low-level/stream-to-string result :err)})))
   (disconnect conn))
 
 (defn -main [& args]
   (let [opts (parse-opts args cli-opts)
-        {:keys [connections]} (model/prepare config)]
+        {:keys [connections]} (model/prepare config)
+        password (if (get-in opts [:options :ask-sudo-pass]) (get-sensitive-user-input "Sudo Password"))]
     (if (get-in opts [:options :help])
       (prn "matross!")
-      (doseq [c connections] (test-run-connection! c "whoami")))
+      (doseq [c connections] (test-run-connection! c "whoami" password)))
     (shutdown-agents)))
