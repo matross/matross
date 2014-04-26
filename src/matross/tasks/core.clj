f0654ed0e2cf9829261ac3fe5bc634d4ee1f4e28
Darrell Hamilton <darrell.noice@gmail.com>
2014-04-25 20:40:30 -0700

    task execution result record

--- src/matross/tasks/core.clj
+++ src/matross/tasks/core.clj
@@ -2,6 +2,11 @@
 
 (defmulti get-module :type)
 
+(defrecord TaskResult [conf succeeded? changed? data])
+
 (defprotocol ITask
   "How to run tasks"
   (exec [self conn]))
+
+(defn task-result [conf succeeded? changed? data]
+  (TaskResult. conf succeeded? changed? data))
