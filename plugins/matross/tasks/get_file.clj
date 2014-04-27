5ced1917ced8af72969ca85f32f30cd879da2ae7
Darrell Hamilton <darrell.noice@gmail.com>
2014-04-26 23:33:11 -0700

    get/put file now operate on streams instead of specifically on files

--- plugins/matross/tasks/get_file.clj
+++ plugins/matross/tasks/get_file.clj
@@ -1,21 +1,16 @@
 (ns matross.tasks.get-file
   (:require [matross.connections.core :refer [run]]
             [matross.tasks.core :refer [task-result get-task ITask]]
-            [me.raynes.conch.low-level :refer (stream-to)]
-            [me.raynes.fs :refer (file exists? expand-home)]))
+            [me.raynes.conch.low-level :refer (stream-to)]))
 
 (deftype GetFile [spec]
   ITask
   (exec [self conn]
-    (let [{:keys [src dest]} spec
-          dest-path (expand-home dest)]
-      (if-not (exists? dest-path)
-        (let [dest-file (file dest-path)
-              cat (str "cat " src)
+    (let [{:keys [src dest]} spec]
+        (let [cat (str "cat " src)
               proc (run conn {:cmd ["/bin/sh" "-c" cat]})]
-          (stream-to proc :out dest-file)
-          (task-result (= 0 @(:exit proc)) true {}))
-        (task-result false false {})))))
+          (stream-to proc :out dest)
+          (task-result (= 0 @(:exit proc)) false  proc)))))
 
 (defmethod get-task :get-file [spec]
   (new GetFile spec))
--- plugins/matross/tasks/put_file.clj
+++ plugins/matross/tasks/put_file.clj
@@ -1,21 +1,16 @@
 (ns matross.tasks.put-file
-  (:require [me.raynes.fs :refer (file exists? expand-home)]
-            [matross.tasks.core :refer [task-result get-task ITask]]
+  (:require [matross.tasks.core :refer [task-result get-task ITask]]
             [matross.connections.core :refer [run]])
   (:import [java.io FileInputStream]))
 
 (deftype PutFile [spec]
   ITask
   (exec [self conn]
-    (let [{:keys [src dest]} spec
-          src-path (expand-home src)]
-      (if (exists? src-path)
-        (let [file-stream (-> src-path file FileInputStream.)
-              cat (str "cat > " dest "")
-              opts {:cmd ["/bin/sh" "-c" cat] :in file-stream}
-              {:keys [exit] :as proc} (run conn opts)]
-          (task-result (= 0 @exit) true {}))
-        (task-result false false {})))))
+    (let [{:keys [src dest]} spec]
+        (let [cat (str "cat > " dest "")
+              opts {:cmd ["/bin/sh" "-c" cat] :in src}
+              proc (run conn opts)]
+          (task-result (= 0 @(:exit proc)) true proc)))))
 
 (defmethod get-task :put-file [spec]
    (new PutFile spec))
