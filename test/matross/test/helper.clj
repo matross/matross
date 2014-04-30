c24889ad5f28bfe5228e0dc046e6406d2e03a5f7
Darrell Hamilton <darrell.noice@gmail.com>
2014-04-29 20:59:42 -0700

    remove redundant vagrant functionality (now in connections/vagrant.clj)

--- test/matross/test/helper.clj
+++ test/matross/test/helper.clj
@@ -1,8 +1,6 @@
 (ns matross.test.helper
   "Helper functions for writing and running tests"
-  (:require [me.raynes.conch :refer [with-programs]]
-            [matross.connections.core :refer [get-connection]]
-            [matross.connections.ssh :refer [ssh-config-connection]]))
+  (:require [me.raynes.conch :refer [with-programs]]))
 
 (defn get-running-vms []
   (with-programs [vagrant grep cut]
@@ -11,8 +9,3 @@
         (vagrant "status" {:seq true})
         {:seq true})
       {:seq true})))
-
-(defn get-vagrant-connection [box-name]
-  (with-programs [vagrant]
-    (let [config (vagrant "ssh-config" box-name)]
-      (-> config ssh-config-connection get-connection))))
