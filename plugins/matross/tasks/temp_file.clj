6e6142ef610a6e346336b3aa56761c3132b0a6a0
Thomas Omans <thomas.omans@gmail.com>
2014-05-01 12:21:40 -0700

    moar macros.. no boggie :scream_cat:

--- plugins/matross/tasks/temp_file.clj
+++ plugins/matross/tasks/temp_file.clj
@@ -1,19 +1,26 @@
 (ns matross.tasks.temp-file
   (:require [matross.tasks.core :refer [deftask run-task task-result]]
+            [clojure.string :refer [trim-newline]]
             [me.raynes.conch.low-level :refer [stream-to-string]]))
 
 
 (deftask :temp-file [conn conf]
-  (let [temp-result (run-task conn {:type :command :command "mktemp"})
-        temp-file (stream-to-string (get temp-result :data) :out)]
-    (task-result (:succeeded? temp-result)
-                 true
-                 (assoc (:data temp-result) :path temp-file))))
+  (let [{:keys [data succeeded?]} (run-task conn {:type :command :command "mktemp"})
+        temp-file (trim-newline (stream-to-string data :out))]
+    (task-result succeeded? true (assoc data :path temp-file))))
 
 (defmacro with-temp-files
-  "Evaluate the body with the names bound to temp files on the given connection,
-cleaning them up after execution."
+  ;; hic sunt dracones
+  "Evaluate the body with the names bound to temp files on the connection"
   [conn bindings & body]
-  `(let [~@(interleave bindings
-                       (map (fn [b#] '(get-in (run-task conn {:type :temp-file}) [:data :path])) bindings))]
-     ~@body))
+  (cond
+    (= (count bindings) 0)
+      `(do ~@body)
+    (= (bindings 0) conn)
+      `(throw (IllegalArgumentException. "Collission between conn and tempfile bindings."))
+    (symbol? (bindings 0))
+      `(let [~(bindings 0) (get-in (run-task ~conn {:type :temp-file}) [:data :path])]
+         (try
+           (with-temp-files ~conn ~(subvec bindings 1) ~@body)
+           (finally (run-task ~conn {:type :command
+                                     :command (str "rm -f " ~(bindings 0))}))))))
