06d56bfe1537fae52dfdc1ac583ac6ff4b15c6e6
Darrell Hamilton <darrell.noice@gmail.com>
2014-04-30 23:18:27 -0700

    task for creating temp files
    Macro works, except for deleting the files.

---
+++ plugins/matross/tasks/temp_file.clj
@@ -0,0 +1,20 @@
+(ns matross.tasks.temp-file
+  (:require [matross.tasks.core :refer [deftask run-task task-result]]
+            [me.raynes.conch.low-level :refer [stream-to-string]]))
+
+
+(deftask :temp-file [conn conf]
+  (let [temp-result (run-task conn {:type :command :command "mktemp"})
+        temp-file (stream-to-string (get temp-result :data) :out)]
+    (task-result (:succeeded? temp-result)
+                 true
+                 (assoc (:data temp-result) :path temp-file))))
+
+(defmacro with-temp-files
+  "Evaluate the body with the names bound to temp files on the given connection,                                                                                                              cleaning them up after execution."
+  [conn bindings & body]
+  `(let [~@(interleave bindings
+                       (map (fn [b#] '(get-in (run-task conn {:type :temp-file}) [:data :path])) bindings))
+         r# '(do ~@body)]
+     '(map (fn [b] (run-task conn {:type :command :command (str "rm -f" b)})) ~bindings)
+     r#))
