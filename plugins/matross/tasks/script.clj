a662277a7108edd0fed95f9dacb73ac8c63b80c4
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-01 09:37:13 -0700

    place a script temporarily on the target machine and run it

---
+++ plugins/matross/tasks/script.clj
@@ -0,0 +1,35 @@
+(ns matross.tasks.script
+  (:require [matross.tasks.core :refer [deftask run-task]]
+            [matross.tasks.template :as template]
+            [matross.tasks.temp-file :refer [with-temp-files]]))
+
+
+(def defaults {:template true
+               :vars {}
+               :env {}})
+
+(defn get-script [conf]
+  (if (:file conf)
+    (slurp (:file conf))
+    (get conf :inline "")))
+
+(defn template-conf [conf src dest]
+  (-> conf
+      (assoc :type :template)
+      (assoc :inline src)
+      (assoc :dest (clojure.string/trim-newline dest))
+      (update-in [:vars] #(if (:template conf) %1 {}))))
+
+(deftask :script [conn conf]
+  (comment
+    :file a file to render
+    :inline a string to render (optional, instead of :file)
+    :template boolean whether or not to template the script (default true)
+    :env environment variables to expose to the script
+    :vars map of vars for templating)
+  (with-temp-files conn [script]
+    (let [conf (merge defaults conf)
+          src (get-script conf)
+          final (run-task conn (template-conf conf src script))]
+      (run-task conn {:type :command :command (str "chmod +x " script)})
+      (run-task conn {:type :command :command script}))))
