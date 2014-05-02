59be6938509d2a4c0d75729f4e7a61b286f26c6f
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-02 15:14:39 -0700

    Better status message

--- scripts/test/integration.sh
+++ scripts/test/integration.sh
@@ -3,6 +3,6 @@
 set -e
 
 for vm in `find $(dirname $0)/../../.test/ssh-configs -type f`; do
-  echo $(basename $vm)
+  echo "Testing against: " $(basename $vm)
   env VAGRANT_SSH_CONF=$vm lein test :integration
 done
