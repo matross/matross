02a4ebc2ef515f814ad09692f8d5a56f16bfbc2d
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-02 15:15:21 -0700

    tidying up and simple box limiting

--- scripts/test/ssh-config-gen.sh
+++ scripts/test/ssh-config-gen.sh
@@ -2,15 +2,20 @@
 
 set -e
 
+BOX_NAME=$1
+
 here=$(dirname $0)
 resources=$here/../../.test/ssh-configs
 
 mkdir -p $resources
 
-vagrant up
+vagrant up $BOX_NAME
 
-vagrant status | grep '(' | awk '{ print $1 }' |
-  xargs -I {} \
+vagrant status \
+    | grep '(' \
+    | grep 'running' \
+    | awk '{ print $1 }' \
+    | xargs -I {} \
     /bin/sh -c "vagrant ssh-config {} > $resources/{}"
 
 echo generated ssh configs: $(ls $resources)
