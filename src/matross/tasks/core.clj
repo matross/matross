bb4a3d417394be14f6fe97599c50d15d98b82bfe
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-03 13:10:08 -0700

    separate the functionality from the multimethod

--- src/matross/tasks/core.clj
+++ src/matross/tasks/core.clj
@@ -12,13 +12,14 @@
   (TaskResult. succeeded? changed? data))
 
 (defmacro deftask [type [conn config] & body]
-  `(defmethod matross.tasks.core/get-task (keyword '~type) [spec#]
-     (reify
-       matross.tasks.core/ITask
-       (exec [_# conn#]
-         (let [~conn conn#
-               ~config spec#]
-           ~@body)))))
+  `(do
+    (defn ~type [~conn ~config] ~@body)
+
+    (defmethod matross.tasks.core/get-task (keyword '~type) [spec#]
+       (reify
+         matross.tasks.core/ITask
+         (exec [_# conn#]
+           (~type conn# spec#))))))
 
 (defn run-task [conn conf]
   (exec (get-task conf) conn))
