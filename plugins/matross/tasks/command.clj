ca624d5b2b781e9e55f8a84c8769fb8f0d8c6539
Thomas Omans <thomas.omans@gmail.com>
2014-05-03 02:15:47 -0700

    a little task cleanup :bath:

--- plugins/matross/tasks/command.clj
+++ plugins/matross/tasks/command.clj
@@ -3,29 +3,23 @@
             [matross.tasks.core :refer [deftask task-result]]
             [matross.connections.core :refer [run exit-ok?]]))
 
-(comment
-:command shell command to run
-:shell system shell to use (optional)
-:env map of environment (optional))
-
 (defn key=val [map-entry]
   (let [[key val] map-entry
         keyname (name key)]
     (str keyname "=" val)))
+
+(defdocs :command
+  {:description "Run a shell command on a remote machine."
+   :url "https://github.com/matross/matross/blob/master/plugins/matross/tasks/command.clj" 
+   :options {:command "command to run"
+             :env "map containing shell environment (optional)"
+             :shell "path to shell to use (optional)"}
+   :defaults {:shell "/bin/sh" :env {}}
+   :examples [{:command "echo $message"
+               :env {:message "hello, world!"}}]})
 
 (deftask :command [conn {:keys [shell command env]
                          :or   {shell "/bin/sh"}}]
   (let [cmd (concat ["/usr/bin/env" "-i"] (map key=val env) [shell "-c" command])
         proc (run conn {:cmd cmd})]
     (task-result (exit-ok? proc) true proc)))
-
-(defdocs :command
-  {:description "Run a shell command on a remote machine."
-   :url "https://github.com/matross/matross/blob/master/plugins/matross/tasks/command.clj" 
-   :options
-     {:command "command to run"
-      :env "map containing shell environment (optional)"
-      :shell "path to shell to use (optional)"}
-   :defaults {:shell "/bin/sh" :env {}}
-   :examples [{:command "echo $message"
-               :env {:message "hello, world!"}}]})
