615aa6f227901b6e5fed7b264d9cf25649da0a43
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-03 15:50:22 -0700

    deftask inputs match defn
    Allows use of doc-strings and metadata in the task definition

--- src/matross/tasks/core.clj
+++ src/matross/tasks/core.clj
@@ -11,15 +11,23 @@
 (defn task-result [succeeded? changed? data]
   (TaskResult. succeeded? changed? data))
 
-(defmacro deftask [type [conn config] & body]
-  `(do
-    (defn ~type [~conn ~config] ~@body)
+(defmacro deftask
+  "Define a function and register it's name as an implementation of the matross.tasks.core/get-task multimethod.
 
-    (defmethod matross.tasks.core/get-task (keyword '~type) [spec#]
-       (reify
-         matross.tasks.core/ITask
-         (exec [_# conn#]
-           (~type conn# spec#))))))
+The arguments are the same as `defn`. The only expectation is that the first argument will be bound to the IRun instance to opereate on and the second argument will be bound to the current configuration."
+
+  [& fdecl]
+
+  (let [type (first fdecl)
+        param_count (count (some #(if (vector? %1) %1) fdecl))]
+    `(do
+       (defn ~@fdecl)
+
+       (defmethod matross.tasks.core/get-task (keyword '~type) [spec#]
+         (reify
+           matross.tasks.core/ITask
+           (exec [_# conn#]
+             (~type conn# spec#)))))))
 
 (defn run-task [conn conf]
   (exec (get-task conf) conn))
