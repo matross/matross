eb85f2bf70bf8e564f545f7d657119bed6d9e4c0
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-25 22:55:23 -0600

    updated tests to reflect new default namespace

--- test/matross/config_test.clj
+++ test/matross/config_test.clj
@@ -2,7 +2,6 @@
   (:require [clojure.test :refer :all]
             [matross.config :refer :all]))
 
-
 (deftest config-resolver-behaves
   (testing "I can retrieve values"
     (let [cr (config-resolver {:ns {:kwd "foo"}})
@@ -11,13 +10,13 @@
       (is (not (= (kwd cr) (:kwd cr))))))
 
   (testing "Templating occurs properly"
-    (let [cr (config-resolver {:default {:foo "{{fact/bar}}" :baz "{{foo}}"}
+    (let [cr (config-resolver {:var {:foo "{{fact/bar}}" :baz "{{foo}}"}
                                :fact {:bar "herp"}})]
       (is (= (:foo cr) ((keyword "fact/bar") cr)))
       (is (= (:foo cr) (:baz cr)))))
 
   (testing "I can use select-keys"
-    (let [cr (config-resolver {:default {:a "{{b}}" :b "value"}})
+    (let [cr (config-resolver {:var {:a "{{b}}" :b "value"}})
           sub-set (select-keys cr [:a])]
       (println sub-set)
       (is (= (:a sub-set) "value")))))
